dbba6d8465a038c8789b1397cea5f11b2d77f671
Tore Sæderup <tore@saederup.net>
2021-07-20 09:54:13 +0200

    Create async-buffering.go

---
+++ async-buffering.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func GetBufferedIntSlice() []int {
+	channel := make(chan int, 100) //TODO: buffer size?
+	response := []int{}
+
+	go dummyAsyncOutput(channel)
+	fmt.Println("waiting for packets")
+	for packet := range channel {
+		fmt.Printf("recieved %v\n", packet)
+		response = append(response, packet)
+
+	}
+	fmt.Println("no more packets")
+	return response
+}
+
+func dummyAsyncutput(outputChannel chan int) {
+	for i := 0; i < 50; i++ {
+		// time.Sleep(500 * time.Millisecond)
+		fmt.Printf("sent %v\n", i)
+		outputChannel <- i
+	}
+	close((outputChannel))
+}
